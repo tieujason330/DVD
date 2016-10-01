0484672dd9727885672d9a904d710b9b65d0e194
Daniel Tenorio <tnr.daniel@gmail.com>
2016-09-30 23:46:46 -0700

    Items picked up set images and the input now works in the inventory menu. The sensitivity needs to be adjusted

--- Assets/Scripts/PlayerScripts/PlayerComponentScripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerScripts/PlayerComponentScripts/PlayerInventory.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -17,11 +18,22 @@ public class PlayerInventory : MonoBehaviour
     public int _yOffset;
 
     public uint _size;
-    public Dictionary<uint, GameObject> _slots;
-    private uint _current;
-    private uint _selected;
+    //public Dictionary<int, GameObject> _slots;
+    //public Dictionary<int, GameObject> _items;
+    public GameObject[,] _slots;
+    private int _currentRow = 0;
+    private int _currentColumn = 0;
+    private int _selected;
     private bool _equipping;
     private bool _swapping;
+    private int _slotCount;
+    private GameObject _currentSlot;
+
+    private float _inputHorizontal;
+    private float _inputVertical;
+
+   public float _inputRate = 0.25f;
+    private float _nextInput = 0.0f;
 
 
     // This should be assigned the canvas gameobject so that we can edit it directly though the editor
@@ -31,15 +43,18 @@ public class PlayerInventory : MonoBehaviour
     void Start()
     {
         _open = false;
-        _slots = new Dictionary<uint, GameObject>();
+        //_slots = new Dictionary<int, GameObject>();
+        _slotCount = _maxColumnCount * _maxRowCount;
+        _slots = new GameObject[_maxRowCount, _maxColumnCount];
 
-        for (int i = 0; i < _maxColumnCount; i++)
+        for (int i = 0; i < _maxRowCount; i++)
         {
-            for (int j = 0; j < _maxRowCount; j++)
+            for (int j = 0; j < _maxColumnCount; j++)
             {
                 GameObject slot = Instantiate(_slotObject);
-                slot.GetComponent<RectTransform>().anchoredPosition = new Vector3(_initialSlotX + (j * _xOffset), _initialSlotY - (i * _yOffset), 0);
+                slot.GetComponent<RectTransform>().anchoredPosition = new Vector3(_initialSlotX + (i * _xOffset), _initialSlotY - (j * _yOffset), 0);
                 slot.transform.SetParent(_inventoryCanvas.transform);
+                _slots[i, j] = slot;
             }
         }
     }
@@ -49,7 +64,7 @@ public class PlayerInventory : MonoBehaviour
     {
         if (_open)
         {
-
+            UpdateInput();
         }
     }
 
@@ -61,15 +76,106 @@ public class PlayerInventory : MonoBehaviour
         }
     }
 
+    // This fucked up shit makes me think we should do something else instead of list indices
+    void UpdateInput()
+    {
+        _inputHorizontal = Input.GetAxis("InventoryHorizontal");
+        _inputVertical = Input.GetAxis("InventoryVertical");
+
+        if (Time.time > _nextInput)
+        {
+            int previousColumn = _currentColumn;
+            int previousRow = _currentRow;
+
+            if (Mathf.Abs(_inputVertical) > 0.5f)
+            {
+                _currentColumn = (_currentColumn + (int)(_inputVertical / Mathf.Abs(_inputVertical))) % _maxColumnCount;
+
+                if (_currentColumn < 0)
+                    _currentColumn = _maxColumnCount - 1;
+
+                print("COLUMN " + _currentColumn);
+            }
+
+            if (Mathf.Abs(_inputHorizontal) > 0.5f)
+            {
+                _currentRow = (_currentRow + (int)(_inputHorizontal / Mathf.Abs(_inputHorizontal))) % _maxRowCount;
+
+                if (_currentRow < 0)
+                    _currentRow = _maxRowCount - 1;
+
+                print("ROW" + _currentRow);
+            }
+
+            if (previousColumn != _currentColumn || previousRow != _currentRow)
+            {
+                _nextInput = Time.time + _inputRate;
+                _currentSlot.GetComponent<RectTransform>().sizeDelta = new Vector3(75, 75, 0);
+                _currentSlot = _slots[_currentRow, _currentColumn];
+                _currentSlot.GetComponent<RectTransform>().sizeDelta = new Vector3(90, 90, 0);
+            }
+        }
+    }
+
     // flip the open variable and return it
     public bool Toggle()
     {
         _open = !_open;
         _inventoryCanvas.SetActive(_open);
+
+        // when the inventory is open the first item in it should be set to 
+        // be the currently selected item
+        if (_open)
+        {
+            _currentColumn = 0;
+            _currentRow = 0;
+            _currentSlot = _slots[0, 0];
+            _currentSlot.GetComponent<RectTransform>().sizeDelta = new Vector3(90, 90, 0);
+        }
+        else
+        {
+            _currentSlot.GetComponent<RectTransform>().sizeDelta = new Vector3(75, 75, 0);
+        }
+
         return _open;
     }
     public void PlayerUpdate()
     {
 
+    }
+
+    public void StoreItem(GameObject pickup)
+    {
+        GameObject openSlot = null;
+
+        // Find the first empy slot in the inventory
+        for (int i = 0; i < _maxColumnCount; i++)
+        {
+            for (int j = 0; j < _maxRowCount; j++)
+            {
+
+                GameObject currentSlot = _slots[j,i];
+                print(currentSlot);
+                if (currentSlot.GetComponent<InventorySlot>()._slotItem == null)
+                {
+                    openSlot = _slots[j, i];
+                }
+
+                // once an empty slot is found we can exit both loops
+                if (openSlot != null)
+                    goto end_of_loop;
+            }
+        }
+        end_of_loop:
+            
+        if (openSlot == null)
+        {
+            print("Not enough room in inventory");
+        }
+        else
+        {
+            openSlot.GetComponent<InventorySlot>().SetItem(pickup);
+            pickup.SetActive(false);
+        }
     }
 }
